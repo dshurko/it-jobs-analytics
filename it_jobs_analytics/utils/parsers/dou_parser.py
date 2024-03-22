a426cb54a1c7437054a03753fe7aa57443a46071
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2024-03-22 02:02:53 +0200

    add DouParser

---
+++ it_jobs_analytics/utils/parsers/dou_parser.py
@@ -0,0 +1,182 @@
+import re
+from datetime import date, datetime
+from typing import Dict, List
+
+import requests
+from base_parser import BaseParser
+from bs4 import BeautifulSoup
+
+
+class DouParser(BaseParser):
+    """
+    A parser for scraping job listings from DOU.
+
+    Attributes:
+        CATEGORY_URL_MAP (dict): A mapping of job categories to their corresponding URLs.
+        JOBS_URL (str): The base URL for job listings.
+        SET_LANG_URL (str): The URL for setting the language to English.
+        USER_AGENT (str): The user agent to use for the HTTP requests.
+    """
+
+    CATEGORY_URL_MAP = {
+        # "AI/ML": "AI/ML",
+        # "Big Data": "Big Data",
+        # "Blockchain": "Blockchain",
+        # "Data Engineer": "Data Engineer",
+        # "Data Science": "Data Science",
+        # "DevOps": "DevOps",
+        # "Front End": "Front End",
+        # "Java": "Java",
+        # "Node.js": "Node.js",
+        "Python": "Python",
+        "Rust": "Rust",
+        "Scala": "Scala",
+    }
+    JOBS_URL = "https://jobs.dou.ua/vacancies/"
+    SET_LANG_URL = "https://dou.ua/?switch_lang=en"
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0"
+
+    def __get_jobs_from_page(self, page_html: str, category: str) -> List[Dict]:
+        """
+        Extracts job listings from a page HTML.
+
+        Args:
+            page_html (str): The HTML content of the page.
+            category (str): The category of the job listings.
+
+        Returns:
+            List[Dict]: A list of job listings as dictionaries.
+        """
+        soup = BeautifulSoup(page_html, "html.parser")
+        li_tags = soup.find_all("li", class_="l-vacancy")
+
+        jobs = []
+
+        for li in li_tags:
+            a_tag = li.find("a", class_="vt")
+            title = a_tag.text.strip()
+            url = a_tag["href"].rstrip("?from=list_hot")
+            company = li.find("a", class_="company").text.strip()
+            date_str = li.find("div", class_="date").text.strip()
+            published_at = datetime.strptime(date_str, "%d %B %Y").date()
+
+            jobs.append(
+                {
+                    "category": category,
+                    "company": company,
+                    "published_at": published_at,
+                    "source": "dou",
+                    "title": title,
+                    "url": url,
+                }
+            )
+
+        return jobs
+
+    def __get_all_jobs_by_category(self, category: str) -> List[Dict]:
+        """
+        Retrieves all job listings for a specific category.
+
+        Args:
+            category (str): The category of the job listings.
+
+        Returns:
+            List[Dict]: A list of job listings as dictionaries.
+        """
+        try:
+            with requests.Session() as session:
+                category_url = (
+                    f"{self.JOBS_URL}?category={self.CATEGORY_URL_MAP[category]}"
+                )
+                headers = {"User-Agent": self.USER_AGENT, "Referer": category_url}
+
+                # set language to English
+                session.get(self.SET_LANG_URL, headers=headers)
+
+                response = session.get(category_url, headers=headers)
+
+                soup = BeautifulSoup(response.text, "html.parser")
+                csrf_token = soup.find("input", {"name": "csrfmiddlewaretoken"})[
+                    "value"
+                ]
+
+                post_url = f"{self.JOBS_URL}xhr-load/?category={self.CATEGORY_URL_MAP[category]}"
+
+                count = 0
+                jobs = []
+
+                while True:
+                    data = {
+                        "csrfmiddlewaretoken": csrf_token,
+                        "count": count,
+                    }
+
+                    post_response = session.post(post_url, data=data, headers=headers)
+                    response_data = post_response.json()
+
+                    page_html = response_data["html"]
+
+                    jobs_from_page = self.__get_jobs_from_page(page_html, category)
+                    jobs.extend(jobs_from_page)
+
+                    if response_data["last"]:
+                        break
+
+                    count += 40
+
+                return jobs
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return []
+
+    def get_jobs_by_category(
+        self, category: str, start_date: date, end_date: date
+    ) -> List[Dict]:
+        """
+        Get job listings by category and within a specified date range.
+
+        Args:
+            category (str): The category of jobs to retrieve.
+            start_date (date): The start date of the date range.
+            end_date (date): The end date of the date range.
+
+        Returns:
+            List[Dict]: A list of job listings as dictionaries.
+        """
+        jobs = [
+            job
+            for job in self.__get_all_jobs_by_category(category)
+            if start_date <= job["published_at"] <= end_date
+        ]
+
+        return jobs
+
+    def get_job_description(self, url: str) -> str:
+        """
+        Get the description of a job listing.
+
+        Args:
+            url (str): The URL of the job listing.
+
+        Returns:
+            str: The description of the job listing.
+        """
+        headers = {"User-Agent": self.USER_AGENT, "Referer": url}
+
+        try:
+            with requests.Session() as session:
+                response = session.get(url, headers=headers)
+
+                soup = BeautifulSoup(response.text, "html.parser")
+                div_tag = soup.find("div", class_="text b-typo vacancy-section")
+
+                description = div_tag.get_text(separator="\n").strip()
+                description = re.sub(r"[ \t]+", " ", description)
+                description = re.sub(r"( *\n *)+", "\n", description)
+
+                return description
+
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            return ""
